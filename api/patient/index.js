d167a45613af912b3e314c6c353b6e2188a78f1b
Dang <ntdang_20th@student.agu.edu.vn>
2023-01-10 14:52:59 +0700

    fd

--- api/patient/index.js
+++ api/patient/index.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 const getListDoctor = async () => {
     try {
-        const doctors = await axios.get('http://10.0.2.2:8000/doctor/doctor/')
+        const doctors = await axios.get('http://10.0.2.2:8000/patient/has-patient-familiar/')
         // console.log(doctors.data)
         return doctors.data
     } catch (error) {
